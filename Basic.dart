f2083b7b914bc0c5e6000e0643c9b128b89489a5
Nayeem Uddin Ahmed Khan <64330403+Khanboyz@users.noreply.github.com>
2021-09-07 17:11:34 +0600

    Create Basic.dart

---
+++ Basic.dart
@@ -0,0 +1,17 @@
+void main() {
+  int a=10;
+  double b=3.30;
+  bool c=true;
+  bool d=false;
+  print("flutter");
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  String add() {
+   return "back";
+  }
+
+  String result = add();
+  print(result);
+}
